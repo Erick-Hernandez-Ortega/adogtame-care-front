fa923a7d4903685eb94deeee0aaab6b420083cd9
Erick Ramon Hernandez Ortega <erickramon47@live.com.mx>
2025-05-28 20:57:40 -0600

    feat(welcome): habilitar rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', loadComponent: () => import('./features/welcome/welcome.component').then((m) => m.WelcomeComponent) },
+];
